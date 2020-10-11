7e7b2ea5ba30d3376ad3638eff02e0a53cc6edf9
Yu Wan <wanyuac@126.com>
2020-10-11 14:02:48 +0100

    Update mergeGenomicRegions.py
    Added support to BED format

--- annotation/mergeGenomicRegions.py
+++ annotation/mergeGenomicRegions.py
@@ -17,12 +17,13 @@ overlapping regions merged.
 Example command (three ways to run this script):
     python mergeGenomicRegions.py -i repeats.coords -o repeats_merged.coords
     python filterCoords.py -i genome.coords -I 90 | python mergeGenomicRegion.py -o repeats_merged.coords
+    python filterCoords.py -i genome.coords -I 90 | python mergeGenomicRegion.py -b -o repeats_merged.bed
     python filterCoords.py -i genome.coords -o repeats.coords -I 90 && python mergeGenomicRegion.py -i repeats.coords -o repeats_merged.coords
 
 Dependencies: Python v3 and packages pandas (pandas.pydata.org/)
 Copyright 2020 Yu Wan <wanyuac@126.com>
 Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
-Publication: 25 Apr 2020; last modification: 1 May 2020.
+Publication: 25 Apr 2020; last modification: 11 Oct 2020.
 """
 
 import sys
@@ -30,9 +31,10 @@ import pandas as pd
 from argparse import ArgumentParser
 
 def parse_arguments():
-    parser = ArgumentParser(description = 'Exclude duplicated region definitions and merge overlapping regions')
-    parser.add_argument('-i', type = str, required = False, default = '', help = 'Path to the input CSV file')
-    parser.add_argument('-o', type = str, required = True, help = 'Path to the output CSV file')
+    parser = ArgumentParser(description = "Exclude duplicated region definitions and merge overlapping regions")
+    parser.add_argument("-i", type = str, required = False, default = "", help = "Path to the input CSV file")
+    parser.add_argument("-o", type = str, required = True, help = "Path to the output CSV file")
+    parser.add_argument("-b", action = "store_true", help = "Print coordinates in BED format rather than position format in the output file")
 
     return parser.parse_args()
 
@@ -42,26 +44,38 @@ def main():
     
     # Import the raw coordinate table
     try:
-        if args.i == '':
-            tab = pd.read_csv(sys.stdin, header = None, names = ['From', 'To'], index_col = None, dtype = int)
+        if args.i == "":
+            tab = pd.read_csv(sys.stdin, header = None, names = ["From", "To"], index_col = None, dtype = int)
         else:
-            tab = pd.read_csv(args.i, header = None, names = ['From', 'To'], index_col = None, dtype = int)
+            tab = pd.read_csv(args.i, header = None, names = ["From", "To"], index_col = None, dtype = int)
     except:
-        print('The input file is not accessible.')
+        print("The input file is not accessible.")
 
     # Remove duplicated rows: equivalent to unique(tab) in R.
-    tab.drop_duplicates(subset = ['From', 'To'], keep = 'first', inplace = True)  # len(tab.index) shows a reduction in the row number.
+    tab.drop_duplicates(subset = ["From", "To"], keep = "first", inplace = True)  # len(tab.index) shows a reduction in the row number.
 
     # Sort regions in an ascending order
-    tab.sort_values(by = ['From', 'To'], ascending = True, inplace = True)
+    tab.sort_values(by = ["From", "To"], ascending = True, inplace = True)
     tab.reset_index(drop = True, inplace = True)
 
     # Merge overlapping regions
-    n = len(tab.index)  # Number of remaining rows (regions)
+    n = len(tab.index)  # Number of remaining rows (regions) after deduplication
     if n > 1:
         tab_merg = mergeRegions(tab, n)
     else:
         tab_merg = tab  # Do not need to merge regions when there is one region left.
+
+    # Convert positions to the BED format?
+    if args.b:
+        """
+        Some software requires the BED format (http://genome.ucsc.edu/FAQ/FAQformat#format1) for input coordinates. The BED format
+        adopts a '0-start, half-open' format rather than the intuitive 'position' format for the ease of calculating region lengths.
+        A detailed comparison of both formats can be found in a blog post:
+        http://genome.ucsc.edu/blog/the-ucsc-genome-browser-coordinate-counting-systems/.
+        The following command converts the intuitive '1-start, fully-closed' position format to the BED format. It substracts one
+        from start positions and keeps end positions the same, as introduced in the blog post.
+        """
+        tab_merg["From"] -= 1
 
     # Write the merged table in a CSV file
     tab_merg.to_csv(args.o, header = False, index = False)
@@ -76,42 +90,42 @@ def mergeRegions(tab, nr):
     """
     n = 0  # Number of rows appended to the output data frame
     r1 = tab.iloc[n]  # Extract the first row (region) from the data frame and get a Pandas Series object
-    ub1 = r1['To']  # Upper boundary of the current region
+    ub1 = r1["To"]  # Upper boundary of the current region
     i_max = nr - 1  # Index of the last region
     for i in range(1, nr):
         r2 = tab.iloc[i]  # Get boundaries of the second region
-        lb2 = r2['From']  # Lower boundary of the new region
-        ub2 = r2['To']  # Upper boundary of the second region
+        lb2 = r2["From"]  # Lower boundary of the new region
+        ub2 = r2["To"]  # Upper boundary of the second region
         
         # Check validity
         if lb2 > ub2:
-            sys.exit('Error: lower bound %d > upper bound %d.' % (lb2, ub2))
+            sys.exit("Error: lower bound %d > upper bound %d." % (lb2, ub2))
         
         # Decision: either merge two regions or add a separate region.
         if lb2 <= (ub1 + 1):
             """
             Two regions overlap or immediately adjacent: merge them into a single one. Here, lb1 <= lb2
-            because the data frame 'tab' has been sorted in an ascending order.
+            because the data frame "tab" has been sorted in an ascending order.
             """
             if ub2 > ub1:
                 """
                 Extend the previous region r1 to ub2; else, do nothing as r2 is embedded in r1.
                 """
                 ub1 = ub2
-                r1['To'] = ub1  # Extend the current region r1 and search for the next region
+                r1["To"] = ub1  # Extend the current region r1 and search for the next region
             
             if i == i_max:  # The last region is reached.
                 if n == 0:  # Has not written any row yet
-                    tab_merg = pd.DataFrame(data = {'From': [r1['From']], 'To': [r1['To']]})  # Initialise the output data frame
+                    tab_merg = pd.DataFrame(data = {"From": [r1["From"]], "To": [r1["To"]]})  # Initialise the output data frame
                 else:
                     tab_merg = tab_merg.append(r1)
         else:  # No overlap
             if n == 0:
                 """
-                This is the first region to be saved in the output data frame: push region 'r1' (may
+                This is the first region to be saved in the output data frame: push region "r1" (may
                 have been extended) into the stack of regions.
                 """
-                tab_merg = pd.DataFrame(data = {'From': [r1['From']], 'To': [r1['To']]})  # Initialise the output data frame
+                tab_merg = pd.DataFrame(data = {"From": [r1["From"]], "To": [r1["To"]]})  # Initialise the output data frame
             else:  # No overlap and n > 0
                 tab_merg = tab_merg.append(r1)
             n += 1
@@ -126,5 +140,5 @@ def mergeRegions(tab, nr):
     return tab_merg
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
