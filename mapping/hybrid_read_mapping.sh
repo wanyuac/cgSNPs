0ea878aa89955caf616c0340142b901c92cef168
Yu Wan <wanyuac@126.com>
2023-07-29 23:49:44 +0100

    Update hybrid_read_mapping.sh

--- mapping/hybrid_read_mapping.sh
+++ mapping/hybrid_read_mapping.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # This pipeline maps paired-end Illumina reads and Nanopore long reads against a reference genome and produces BAM files for variant calling.
+# Dependency: samtools v1.17 and later versions, because earlier versions do not have option '-o'.
 # Copyright (C) 2023 Yu Wan <wanyuac@126.com>
 # Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
-# Release: 11 Jan 2023; latest update: 11 Jan 2023
+# Release: 11 Jan 2023; latest update: 29 July 2023
 
 # Help information ###############
 show_help() {
@@ -76,19 +77,25 @@ minimap2 --MD -a -x map-ont -t $t $r $l > ${s}_nanopore.sam
 echo 'Creating BAM files sorted by positions'
 samtools view -S -b -u -@ $t ${s}_illumina.sam | samtools sort -@ $t -o ${s}_illumina_sorted.bam -
 samtools view -S -b -u -@ $t ${s}_nanopore.sam | samtools sort -@ $t -o ${s}_nanopore_sorted.bam -
-samtools index -@ $t ${s}_illumina_sorted.bam
-samtools index -@ $t ${s}_nanopore_sorted.bam
+if [ -f "${s}_illumina_sorted.bam" ] && [ -f "${s}_nanopore_sorted.bam" ]; then
+    samtools index -@ $t ${s}_illumina_sorted.bam
+    samtools index -@ $t ${s}_nanopore_sorted.bam
+else
+    echo "Error: BAM files ${s}_illumina_sorted.bam and/or ${s}_nanopore_sorted.bam were not found." >&2
+    exit
+fi
 
 # Merge BAM file ===============
 # Sort SAM files by read names rather than mapped positions to solve the error of "No @HD tag found".
 echo 'Merging BAM files'
 samtools view -S -b -u -@ $t ${s}_illumina.sam | samtools sort -n --no-PG -@ $t --output-fmt BAM -o ${s}_illumina_name_sorted.bam -
 samtools view -S -b -u -@ $t ${s}_nanopore.sam | samtools sort -n --no-PG -@ $t --output-fmt BAM -o ${s}_nanopore_name_sorted.bam -
-samtools merge -n -f --no-PG --output-fmt BAM -@ $t --reference $r -u -o ${s}_hybrid.bam ${s}_illumina_name_sorted.bam ${s}_nanopore_name_sorted.bam
-samtools sort -@ $t --output-fmt BAM -o ${s}_hybrid_sorted.bam ${s}_hybrid.bam
-samtools index -@ $t ${s}_hybrid_sorted.bam
-
-# Clearance ===============
-rm ${s}_illumina.sam ${s}_nanopore.sam ${s}_illumina_name_sorted.bam ${s}_nanopore_name_sorted.bam ${s}_hybrid.bam
-
-echo 'Successfully went through this pipeline.'
+if [ -f "${s}_illumina_name_sorted.bam" ] && [ -f "${s}_nanopore_name_sorted.bam" ]; then
+    samtools merge -n -f --no-PG --output-fmt BAM -@ $t --reference $r -u -o ${s}_hybrid.bam ${s}_illumina_name_sorted.bam ${s}_nanopore_name_sorted.bam  # It's unlikely that BAM files are not produced in this and next commands at this stage.
+    samtools sort -@ $t --output-fmt BAM -o ${s}_hybrid_sorted.bam ${s}_hybrid.bam
+    samtools index -@ $t ${s}_hybrid_sorted.bam
+    rm ${s}_illumina.sam ${s}_nanopore.sam ${s}_illumina_name_sorted.bam ${s}_nanopore_name_sorted.bam ${s}_hybrid.bam  # Clean-up
+    echo 'Successfully went through this pipeline.'
+else
+    echo "Error: BAM files ${s}_illumina_name_sorted.bam and/or ${s}_nanopore_name_sorted.bam were not found." >&2
+fi
